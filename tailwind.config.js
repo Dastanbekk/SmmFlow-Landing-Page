3538a61deb581b1eae869d4ebdeec53e9effb455
DostonbekZaripov <dastandev0110@gmail.com>
2024-11-21 02:12:40 +0500

    Loyiha batamom tugallandi

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Rubik:'Rubik, sans-serif',
+      },
+      colors: {
+        '1': '#1D4ED8',
+      },
+    },
+  },
+  plugins: [],
+}
